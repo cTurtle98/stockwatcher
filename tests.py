5544f3b400600247eeb3d39193ef3dbcd7e78f0e
cTurtle98 <ciaran@cturtle98.com>
2022-03-04 05:55:59 -0800

    test with 4 ptz

--- tests.py
+++ tests.py
@@ -8,3 +8,6 @@ simple script for testing my package
 
 import stockwatcher
 
+test_url_store_ui_com_in_stock = "https://store.ui.com/collections/unifi-protect/products/unifi-protect-g4-ptz"
+
+print(stockwatcher.scraper.scrape(test_url_store_ui_com_in_stock))
