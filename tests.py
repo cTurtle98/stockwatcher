6008c041f297fb276db6dc2be6a70f986690777e
cTurtle98 <ciaran@cturtle98.com>
2022-03-04 06:32:04 -0800

    add test for out of stock

--- tests.py
+++ tests.py
@@ -10,4 +10,15 @@ import stockwatcher
 
 test_url_store_ui_com_in_stock = "https://store.ui.com/collections/unifi-protect/products/unifi-protect-g4-ptz"
 
-print(stockwatcher.scraper.scrape(test_url_store_ui_com_in_stock))
+test_url_store_ui_com_out_of_stock = "https://store.ui.com/collections/unifi-protect/products/unifi-video-g3-flex-camera"
+
+if stockwatcher.scraper.scrape(test_url_store_ui_com_in_stock) == 1:
+    print("succes in stock")
+else:
+    print("error in stock")
+
+if stockwatcher.scraper.scrape(test_url_store_ui_com_out_of_stock) == 0:
+    print("succes out of stock")
+else:
+    print("error out of stock")
+
