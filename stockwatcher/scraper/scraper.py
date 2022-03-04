2a0c576c99bd8ff6cf718663a0ea03e8e5db643e
cTurtle98 <ciaran@cturtle98.com>
2022-03-04 04:12:45 -0800

    remove class

--- stockwatcher/scraper/scraper.py
+++ stockwatcher/scraper/scraper.py
@@ -9,34 +9,32 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 
-class scraper:
+def main(url):
+    """
+    finds stock status for url
+    return 1 = in stock
+    return 0 = out of stock
+    return -1 = store not supported or error
+    """
 
-    def main(url):
-        """
-        finds stock status for url
-        return 1 = in stock
-        return 0 = out of stock
-        return -1 = store not supported or error
-        """
-
-        if "store.ui.com" in url:
-            return store_ui_com(url)
-        else:
-            return("-1")
-
-    def store_ui_com(url):
-        """
-        scraper for ubiquiti store
-        """
-
-        # load the url
-        driver.get(url)
-
-        # find the add to cart button div
-        product_button = driver.find_element_by_class_name("comProduct_button")
-        if "Add to Cart" in product_button:
-            return("1")
-        if "Sold Out" in product_button:
-            return("0")
-        
+    if "store.ui.com" in url:
+        return store_ui_com(url)
+    else:
         return("-1")
+
+def store_ui_com(url):
+    """
+    scraper for ubiquiti store
+    """
+
+    # load the url
+    driver.get(url)
+
+    # find the add to cart button div
+    product_button = driver.find_element_by_class_name("comProduct_button")
+    if "Add to Cart" in product_button:
+        return("1")
+    if "Sold Out" in product_button:
+        return("0")
+    
+    return("-1")
